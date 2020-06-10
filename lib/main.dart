0a9c41afb5fbb4546f2b674d7b30354499d62019
kharioki <antolisah@gmail.com>
2020-06-11 02:10:52 +0300

    geolocator service and using futureprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:parking_locator/screens/search.dart';
+import 'package:parking_locator/services/geolocator_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final locatorService = GeoLocatorService();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Parking Locator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return FutureProvider(
+      create: (context) => locatorService.getLocation(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Parking Locator',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Search(),
       ),
-      home: Search(),
     );
   }
 }
